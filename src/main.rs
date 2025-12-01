217556807d7ac1e3b5dd4fe93853b8bb262e308d
nickjjarrett <nicholasjjarrett@gmail.com>
2025-12-01 21:42:13 +0000

    Tidied up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
  
 fn main() {
-    let file = fs::read_to_string("C:/Users/nick-/Documents/Code/Rust/Advent2025/src/data.txt")
+    let file = fs::read_to_string("src/data.txt")
         .expect("Should have been able to read the file");
- 
+
     let split = file.split('\n');
     let mut number: i16 = 50;
     let mut code = 0;
@@ -13,27 +13,25 @@ fn main() {
         let instr = line.split_at(1);
         let clicks = instr.1.trim().parse::<i16>().unwrap() ;
         prev = number;
- 
+        
+        // Increment full rotations
         code += clicks/100;
  
-        if instr.0 == "L" {number -= (clicks % 100) }else{number += (clicks%100)}
+        // Increment / decrement value
+        if instr.0 == "L" {number -= clicks % 100}else{number += clicks%100}
        
+        // +ve -> -ve crossover point e.g. 5 -> -9
         if (number.is_positive() && prev.is_negative()) || (prev.is_positive() && number.is_negative())
         {
             code+=1;
         }
- 
-        if (number % 100) != 0 && (prev % 100) != 0
+        // Crossover point, e.g. 93 -> 107
+        else if (number % 100) != 0 && (prev % 100) != 0
         {
             code += ((number / 100) - (prev / 100)).abs();
         }
-        // Need to check for crossover
-        // Change of sign indicates a crossover
-        // ((number / 100) - (prev / 100)).abs()
- 
-        // Increment based on 100 multiples (full rotations)
-        // Check for sign change
-        if number % 100 == 0
+        // Final position is zero
+        else if number % 100 == 0
         {
             code+=1;
         }
