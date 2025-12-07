83587e0d5031167631fbb9846a3cb253677481f3
nickjjarrett <nicholasjjarrett@gmail.com>
2025-12-07 10:14:46 +0000

    Day 5 two gold stars

--- src/day5/solution.rs
+++ src/day5/solution.rs
@@ -44,19 +44,35 @@ pub fn part1(path: &str)
     let mut valid_nums: Vec<usize> = vec![];
     // Part 2
     // Consolidate ranges
-    for range in &ranges
+    // No brute forcing
+    let mut consolidated = consolidate_ranges(ranges);
+    
+    let mut prev = 1000;
+    // consolidated = consolidate_ranges(consolidated);
+    // consolidated = consolidate_ranges(consolidated);
+
+    loop
     {
-        println!("New Range");
-        for i in range.0..range.1+1{
-            if !valid_nums.contains(&i)
-            {
-                valid_nums.push(i);
-            }
+        consolidated = consolidate_ranges(consolidated);
+        if prev - consolidated.len() == 0
+        {
+            break;
         }
+        prev = consolidated.len();
     }
-    println!("Num: {}", valid_nums.len());
 
+    
+    let mut total = 0;
+    
+    for i in consolidated
+    {
+        total += i.1 - i.0 + 1;
+    }
+    
+    println!("Total {}",total);
 
+    // 403440257222619 is too high 
+    // 350939902751909
 
 }
 
@@ -65,4 +81,57 @@ fn parse_ranges(range: &str) -> (usize, usize)
     let vals: Vec<&str> = range.split('-').collect();
     
     return (vals[0].trim().parse::<usize>().expect("asd"), vals[1].trim().parse::<usize>().expect("asd"));
+}
+
+
+fn consolidate_ranges(ranges: Vec<(usize, usize)>) -> Vec<(usize, usize)>
+{
+    let mut consolidated: Vec<(usize, usize)> = vec![ranges[0]];
+
+    for range in &ranges[1..]
+    {
+        let mut found = false;
+        // Check in the consolidated ranges where this one fits in
+        for con_range in &mut consolidated
+        {
+            // Range is fully encapsulated
+            if range.0 >= con_range.0 && range.1 <= con_range.1
+            {
+                found = true;
+                break;
+            }
+            // Range overlaps above
+            else if range.0 >= con_range.0 && range.0 <= con_range.1 && range.1 > con_range.1
+            {
+                found = true;
+                con_range.1 = range.1;
+                break;
+            }
+            // Range overlaps below
+            else if range.0 < con_range.0 && range.1 <= con_range.1 && range.1 >= con_range.0
+            {
+                found = true;
+                con_range.0 = range.0;
+                break;
+            }
+            // Range fully encapsulates
+            else if range.0 <= con_range.0 && range.1 >= con_range.1
+            {
+                found = true;
+                con_range.1 = range.1;
+                con_range.0 = range.0;
+                break;
+            }
+            // Range doesn't overlap, check the next one
+        }
+        if !found
+        {
+            // Need new range added
+            consolidated.push(*range);
+        }
+    }
+
+    // consolidated.sort();
+
+    return consolidated;
 }
